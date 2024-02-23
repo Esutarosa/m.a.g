d2248bd6bd700e5a70ac7e2858705630d237b855
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-02-23 17:54:29 +0200

    404 page, new semi font and fixed footer classes

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Lato, Cousine } from "next/font/google";
+import { Inter, Lato, Cousine, Noto_Sans_JP } from "next/font/google";
 
 // Main font
 export const inter = Inter({ 
@@ -13,6 +13,12 @@ export const lato = Lato({
 
 // Text font
 export const cousine = Cousine({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+// 404 and other designer stuff
+export const notoJP = Noto_Sans_JP({
   subsets: ["latin"],
   weight: ["400", "700"],
 });
