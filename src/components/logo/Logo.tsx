cae2987c19918188c5c31255214a7a0128d22d2c
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-02-27 17:19:11 +0200

    Added logo and favicon

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,9 +1,16 @@
+import Image from "next/image"
 import Link from "next/link"
 
 const Logo = () => {
   return (
-    <Link href="/" className="text-3xl">
-      Logo
+    <Link href="/">
+      <Image
+        src="/logo.png"
+        alt="logo"
+        width={80}
+        height={80}
+        className="bg-primary lg:hidden p-2"
+      />
     </Link>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import Links from "./navLinks/NavLinks"
+import Logo from '../logo/Logo'
 
 const Navbar = () => {
   const [header, setHeader] = useState(false)
@@ -11,19 +12,20 @@ const Navbar = () => {
       window.scrollY > 380 ? setHeader(true) : setHeader(false)
     }
     window.addEventListener('scroll', scrollHandler)
-  
+
     return () => {
       window.removeEventListener('scroll', scrollHandler)
     }
   }, [])
 
   return (
-    <header className={`w-full z-50 ${header ? 
-      'lg:sticky lg:top-0 lg:left-0 lg:bg-background/95 lg:backdrop-blur lg:supports-[backdrop-filter]:bg-background/90 transition-all duration-100' 
-      : 
+    <header className={`w-full z-50 ${header ?
+      'lg:sticky lg:top-0 lg:left-0 lg:bg-background/95 lg:backdrop-blur lg:supports-[backdrop-filter]:bg-background/90 transition-all duration-100'
+      :
       ''} sticky lg:static top-0 left-0 border-b border-border/40 bg-background lg:bg-transparent h-[4rem] shadow-md`}
     >
-      <nav className="flex justify-center py-2.5">
+      <nav className="flex justify-between lg:justify-center px-2.5 py-2.5">
+        <Logo />
         <Links />
       </nav>
     </header>
