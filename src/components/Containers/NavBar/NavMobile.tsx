647016695303b8b0b6d360e99bdd4b080692f6c8
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-16 00:59:13 +0300

    * Finished mobile menu with styles

--- src/components/Containers/NavBar/NavMobile.tsx
+++ src/components/Containers/NavBar/NavMobile.tsx
@@ -1,8 +1,12 @@
 import type { FC, HTMLAttributeAnchorTarget } from 'react';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 import NavItem from './NavItem';
-import { perspective } from '@/anims/animMobileMenu';
+import { perspective, slideIn } from '@/anims/animMobileMenu';
+
+import BandCamp from '@/components/Icons/Social/BandCamp';
+import Twitter from '@/components/Icons/Social/Twitter';
 
 interface NavMobileProps {
   navMobileItems: Array<{
@@ -43,6 +47,46 @@ const NavMobile: FC<NavMobileProps> = ({
           </div>
         ))}
       </div>
+
+      <motion.div
+        className='flex flex-wrap items-center justify-between px-6 pb-6 gap-6'
+      >
+        <motion.div
+          key='twitter'
+          variants={slideIn}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <Link
+            className='text-accent'
+            href='https://twitter.com/AndroidGyaru'
+            aria-label='Twitter'
+          >
+            <Twitter
+              className='w-8 h-8'
+            />
+          </Link>
+        </motion.div>
+
+        <motion.div
+          key='bandcamp'
+          variants={slideIn}
+          initial="initial"
+          animate="enter"
+          exit="exit"
+        >
+          <Link
+            className='text-accent'
+            href='https://modernizedandroidgyaru.bandcamp.com/'
+            aria-label='BandCamp'
+          >
+            <BandCamp
+              className='w-10 h-10'
+            />
+          </Link>
+        </motion.div>
+      </motion.div>
     </nav>
   )
 };
