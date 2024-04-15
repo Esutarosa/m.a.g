afbca25e920880a1e50e768e6bbdab176df2e498
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-16 02:17:46 +0300

    * Animation improvements

--- src/anims/animMobileMenu.ts
+++ src/anims/animMobileMenu.ts
@@ -1,21 +1,23 @@
 export const perspective = {
   initial: {
     opacity: 0,
-    rotateX: 90,
-    translateY: 80,
-    translateX: -20,
+    rotateX: 45,
+    translateY: 70,
+    translateX: -15,
   },
 
-  enter: (i: number) => ({
+  enter: () => ({
     opacity: 1,
     rotateX: 0,
     translateY: 0,
     translateX: 0,
     transition: {
-      duration: 1.05,
-      delay: 0.5 + (i * 0.1),
+      duration: 0.65,
+      delay: 0.5,
       ease: [.215, .61, .355, 1],
-      opacity: { duration: 0.35 }
+      opacity: {
+        duration: 0.35
+      }
     }
   }),
 
@@ -32,15 +34,15 @@ export const perspective = {
 export const slideIn = {
   initial: {
     opacity: 0,
-    y: 20
+    y: 220,
   },
 
-  enter: (i: number) => ({
+  enter: () => ({
     opacity: 1,
     y: 0,
     transition: {
-      duration: 0.5,
-      delay: 0.75 + (i * 0.1),
+      duration: 0.55,
+      delay: 0.65,
       ease: [.215, .61, .355, 1]
     }
   }),
