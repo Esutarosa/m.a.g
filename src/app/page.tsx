dac8a3f9f0e5f972e8b320d5388094429b1786c8
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-04-18 06:53:55 +0300

    - Removed float shape due to uselessness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { FC } from 'react'
+import type { FC } from 'react'
 
 const Home: FC = () => {
   return (
-    <>
-
-    </>
+    <main className='flex items-center justify-center h-[80vh]'>
+      <div className='text-center'>
+        <h1 className='text-5xl'>Modernized Android Gyaru</h1>
+        <p className='text-2xl mt-4 text-muted-foreground font-[100]'>Official website</p>
+      </div>
+    </main>
   )
 }
 
