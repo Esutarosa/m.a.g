f674a038c21c7d8528c77b4a6683e4354eb1f29a
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-03 22:52:39 +0300

    + Contact and discography pages skeleton

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,10 @@
-import { FC } from 'react'
+import type { FC } from 'react'
 
 const Contact: FC = () => {
   return (
-    <div>
-      Contact
-    </div>
+    <main className='flex items-center justify-center h-[80vh]'>
+      <p className='text-muted-foreground'>Something interesting should appear here soon...</p>
+    </main>
   )
 }
 
