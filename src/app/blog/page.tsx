67c66368ea921e6acb56e97275f9550aeeacca5f
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-05-03 22:50:01 +0300

    + Blog page skeleton

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,10 +1,20 @@
-import { FC } from 'react'
+import Link from 'next/link'
+import type { FC } from 'react'
 
 const Blog: FC = () => {
   return (
-    <div>
-      Blog
-    </div>
+    <main className='flex items-center justify-center h-[80vh] container text-center'>
+      <p className='text-muted-foreground'>
+        Interesting information will appear here in the near future,
+        but in the meantime you can follow me on {' '}
+        <Link
+          href='https://twitter.com/AndroidGyaru'
+          className='underline underline-offset-4'
+        >
+          Twitter
+        </Link>
+      </p>
+    </main>
   )
 }
 
