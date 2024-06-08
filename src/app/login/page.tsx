4e332e016bb7706eed3e76256b106cbebd277039
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-06-09 02:12:39 +0300

    + login action button and error handler (simple)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,6 @@
 import type { FC } from 'react';
+import { createClient } from '@/config/supabase/server';
+import { redirect } from 'next/navigation';
 
 import DefaultLayout from '@/components/Layouts/Default';
 import SectionContainer from '@/components/Layouts/SectionContainer';
@@ -7,10 +9,29 @@ import LoginForm from '@/components/LoginForm';
 interface LoginProps { searchParams: { message: string } }
 
 const Login: FC<LoginProps> = ({ searchParams }) => {
+  const login = async (formData: FormData) => {
+    'use server';
+
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    const supabase = createClient();
+
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return redirect('/login?message=Could not authenticate user');
+    }
+
+    return redirect('/admin');
+  }
+
   return (
     <DefaultLayout isHideFooter isHideHeader className='justify-center'>
       <SectionContainer className='flex justify-center'>
-        <LoginForm />
+        <LoginForm searchParams={searchParams} formAction={login} />
       </SectionContainer>
     </DefaultLayout>
   );
--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -6,12 +6,13 @@ import FieldSetWithStatus from '@/components/FieldSetWithStatus';
 import SubmitButtonWithStatus from '@/components/SubmitButtonWithStatus';
 
 interface LoginFormProps {
-  formAction?: (formData: FormData) => Promise<{
+  formAction: (formData: FormData) => Promise<{
     error?: string, success?: boolean
   }>
+  searchParams: { message: string }
 }
 
-const LoginForm: FC<LoginFormProps> = ({ formAction }) => {
+const LoginForm: FC<LoginFormProps> = ({ formAction, searchParams }) => {
   return (
     <InfoBlock className='w-[calc(100vw-2rem)] sm:w-[min(520px,90vw)] px-6 py-5'>
       <h1 className='h3 flex gap-3 items-center justify-center self-start mb-4'>
@@ -38,9 +39,16 @@ const LoginForm: FC<LoginFormProps> = ({ formAction }) => {
               required
             />
           </div>
-          <SubmitButtonWithStatus formAction={formAction}>
-            Login
-          </SubmitButtonWithStatus>
+          <div className='flex gap-4 items-center'>
+            <SubmitButtonWithStatus formAction={formAction}>
+              Login
+            </SubmitButtonWithStatus>
+            {searchParams && searchParams.message && (
+              <p className='text-destructive'>
+                {searchParams.message}
+              </p>
+            )}
+          </div>
         </div>
       </form>
     </InfoBlock>
