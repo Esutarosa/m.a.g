107ef729505b0d131b265405321bd11e4d50a0fc
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-06-12 18:25:34 +0300

    + add other admin pages

---
+++ src/app/admin/blog/page.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react';
+import { redirect } from 'next/navigation';
+import { getUser } from '@/config/store/user';
+import { createClient } from '@/config/supabase/server';
+import AdminLayout from '@/components/Layouts/Admin';
+import AdminSectionContainer from '@/components/Layouts/AdminSectionContainer';
+
+interface BlogProps { }
+
+const Blog: FC<BlogProps> = async ({ }) => {
+  const { user } = await getUser();
+  if (!user) return redirect('/login');
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/");
+  };
+  return (
+    <AdminLayout user={user} signOutAction={signOut}>
+      <AdminSectionContainer>
+
+      </AdminSectionContainer>
+    </AdminLayout>
+  );
+}
+
+export default Blog;
---
+++ src/app/admin/contact/page.tsx
@@ -0,0 +1,30 @@
+import type { FC } from 'react';
+import { redirect } from 'next/navigation';
+import { getUser } from '@/config/store/user';
+import { createClient } from '@/config/supabase/server';
+import AdminLayout from '@/components/Layouts/Admin';
+import AdminSectionContainer from '@/components/Layouts/AdminSectionContainer';
+
+interface ContactProps { }
+
+const Contact: FC<ContactProps> = async ({ }) => {
+  const { user } = await getUser();
+  if (!user) return redirect('/login');
+
+  const signOut = async () => {
+    "use server";
+
+    const supabase = createClient();
+    await supabase.auth.signOut();
+    return redirect("/");
+  };
+  return (
+    <AdminLayout user={user} signOutAction={signOut}>
+      <AdminSectionContainer>
+
+      </AdminSectionContainer>
+    </AdminLayout>
+  );
+}
+
+export default Contact;
