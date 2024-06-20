104f903e0c41541ced73cedc4d4f3b1f9dabc6ae
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-06-20 23:23:52 +0300

    + skeleton for blog edit

--- src/components/Admin/Blog/BlogTableActions.tsx
+++ src/components/Admin/Blog/BlogTableActions.tsx
@@ -12,6 +12,7 @@ import { Button } from '@/components/ui/button';
 import RenderSVG from '@/components/RenderSVG';
 import { toast } from '@/components/ui/use-toast';
 import { deleteBlogById } from '@/config/actions/blog';
+import Link from 'next/link';
 
 interface BlogTableActionsProps { id: string }
 
@@ -61,12 +62,17 @@ const BlogTableActions: FC<BlogTableActionsProps> = ({ id }) => {
           />
           <span>View</span>
         </DropdownMenuItem>
-        <DropdownMenuItem className='cursor-pointer flex items-center gap-2'>
-          <RenderSVG
-            icon='M15.7279 9.57627L14.3137 8.16206L5 17.4758V18.89H6.41421L15.7279 9.57627ZM17.1421 8.16206L18.5563 6.74785L17.1421 5.33363L15.7279 6.74785L17.1421 8.16206ZM7.24264 20.89H3V16.6473L16.435 3.21231C16.8256 2.82179 17.4587 2.82179 17.8492 3.21231L20.6777 6.04074C21.0682 6.43126 21.0682 7.06443 20.6777 7.45495L7.24264 20.89Z'
-            className='size-4'
-          />
-          <span>Edit</span>
+        <DropdownMenuItem className='cursor-pointer'>
+          <Link
+            href={`/admin/blog/edit/${id}`}
+            className='w-full flex items-center gap-2'
+          >
+            <RenderSVG
+              icon='M15.7279 9.57627L14.3137 8.16206L5 17.4758V18.89H6.41421L15.7279 9.57627ZM17.1421 8.16206L18.5563 6.74785L17.1421 5.33363L15.7279 6.74785L17.1421 8.16206ZM7.24264 20.89H3V16.6473L16.435 3.21231C16.8256 2.82179 17.4587 2.82179 17.8492 3.21231L20.6777 6.04074C21.0682 6.43126 21.0682 7.06443 20.6777 7.45495L7.24264 20.89Z'
+              className='size-4'
+            />
+            <span>Edit</span>
+          </Link>
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => deleteBlogPost(id)}
--- src/config/actions/blog.ts
+++ src/config/actions/blog.ts
@@ -4,9 +4,8 @@ import { BlogFormSchema } from "@/components/Admin/Blog/BlogFormSchema";
 import { revalidatePath } from "next/cache";
 import { createClient } from "@/config/supabase/server";
 
-const supabase = createClient();
-
 export async function createBlog(data: BlogFormSchema) {
+  const supabase = await createClient();
   const { ['content']: exeludedKey, ...blog } = data;
 
   const resultBlog = await supabase
@@ -14,7 +13,7 @@ export async function createBlog(data: BlogFormSchema) {
     .insert(blog)
     .select('id')
     .single();
-  revalidatePath('/admin/blog')
+  revalidatePath('/admin/blog');
 
   if (resultBlog.error) {
     return JSON.stringify(resultBlog)
@@ -25,34 +24,43 @@ export async function createBlog(data: BlogFormSchema) {
         blog_id: resultBlog.data.id!,
         content: data.content
       });
-
     return JSON.stringify(result);
   }
 }
 
 export async function readBlog() {
+  const supabase = await createClient();
   return supabase
     .from('blog')
     .select('*')
-    .order('created_at', { ascending: false })
+    .order('created_at', { ascending: false });
 }
 
 export async function deleteBlogById(id: string) {
+  const supabase = await createClient();
   const result = await supabase
     .from('blog')
     .delete()
-    .eq('id', id)
-  revalidatePath('/admin/blog')
-
-  return JSON.stringify(result)
+    .eq('id', id);
+  revalidatePath('/admin/blog');
+  return JSON.stringify(result);
 }
 
 export async function updateBlogById(id: string, data: BlogFormSchema) {
+  const supabase = await createClient();
   const result = await supabase
     .from('blog')
     .update(data)
-    .eq('id', id)
-  revalidatePath('/admin/blog')
+    .eq('id', id);
+  revalidatePath('/admin/blog');
+  return JSON.stringify(result);
+}
 
-  return JSON.stringify(result)
+export async function readBlogContentById(id: string) {
+  const supabase = await createClient();
+  return supabase
+    .from('blog')
+    .select('*,blog_content(*)')
+    .eq('id', id)
+    .single();
 }
