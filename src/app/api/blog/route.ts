86af7d5fbeded8ea266c5b667afda60a85f6fa73
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-06-22 19:28:42 +0300

    feat(api): add get request handler for fetching blog data

---
+++ src/app/api/blog/route.ts
@@ -0,0 +1,30 @@
+import { Database } from "@/config/types/blog";
+import { createClient } from "@supabase/supabase-js";
+
+export async function GET(request: Request) {
+  const supabase = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_KEY!,
+  );
+
+  const { searchParams } = new URL(request.url);
+
+  const id = searchParams.get("id");
+
+  if (id === "*") {
+    const result = await supabase
+      .from("blog")
+      .select("id")
+      .limit(5);
+    return Response.json({ ...result });
+  } else if (id) {
+    const result = await supabase
+      .from("blog")
+      .select("*")
+      .eq("id", id)
+      .limit(5);
+    return Response.json({ ...result });
+  }
+
+  return Response.json({});
+}
