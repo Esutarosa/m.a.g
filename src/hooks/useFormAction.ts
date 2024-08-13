571600404fca98bdc4d4a2f117e800ded5fcdbca
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-08-13 23:41:49 +0300

    Docs for custom hook useFormAction

--- src/hooks/useFormAction.ts
+++ src/hooks/useFormAction.ts
@@ -1,8 +1,21 @@
 import { useState } from 'react';
 
+/**
+ * A hook for executing an asynchronous action and storing the result.
+ * 
+ * @template T - The type of data passed to the action.
+ * @param {(data: T) => Promise<any>} action - A function that takes data of type `T` and returns a Promise.
+ * @returns {[any, (data: T) => Promise<any>]} - An array with the result and a function to execute the action.
+ */
 const useFormAction = <T>(action: (data: T) => Promise<any>) => {
   const [response, setResponse] = useState<any>(null);
 
+  /**
+   * Executes the action and stores the result or error.
+   * 
+   * @param {T} data - Data for the action.
+   * @returns {Promise<any>} - The result of the action.
+   */
   const execute = async (data: T) => {
     try {
       const result = await action(data);
