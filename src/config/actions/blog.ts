a84afbd45947fbc1867697390ddf97a51caa442e
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-08-30 20:56:14 +0300

    feat: add createBlog action

--- src/config/actions/blog.ts
+++ src/config/actions/blog.ts
@@ -2,6 +2,10 @@
 
 import { createClient } from '@/config/supabase/server';
 
+import { BlogFormSchema } from '@/config/definitions/BlogFormSchema';
+
+import { revalidatePath } from 'next/cache';
+
 export async function readBlog() {
   const supabase = await createClient();
 
@@ -33,4 +37,40 @@ export async function readAdminBlog() {
   }
 
   return { data, error: null };
+};
+
+export async function createBlog(data: BlogFormSchema) {
+  const supabase = await createClient();
+
+  const { content, ...blog } = data;
+
+  const { data: newBlog, error: blogError } = await supabase
+    .from('blog')
+    .insert(blog)
+    .select('id')
+    .single();
+  revalidatePath('/admin/blog');
+
+  if (blogError) {
+    return JSON.stringify(blogError);
+  }
+
+  if (newBlog) {
+    const { data: blogContentResult, error: contentError } = await supabase
+      .from('blog_content')
+      .insert({
+        blog_id: newBlog.id!,
+        content
+      });
+
+    if (contentError) {
+      return JSON.stringify(contentError);
+    }
+
+    return JSON.stringify(blogContentResult);
+  }
+
+  return JSON.stringify({
+    error: 'Blog creation failed without specific error'
+  });
 };
