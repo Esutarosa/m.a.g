414795e85a7e809f416f5af96f1d83be6ee2e516
Esu <107695658+Esutarosa@users.noreply.github.com>
2024-08-31 12:28:10 +0300

    refactor: remove unnecessary item animations from admin menu for simplification

--- src/components/admin/AdminSidebar.tsx
+++ src/components/admin/AdminSidebar.tsx
@@ -10,8 +10,6 @@ import Link from 'next/link';
 
 import SVG from '@/components/SVG';
 
-import AnimateItems from '@/components/AnimateItems';
-
 import { LOGOUT } from '@/config/icons';
 
 import { signOut } from '@/config/actions/auth';
@@ -20,9 +18,8 @@ const AdminSidebar: FC = () => {
   return (
     <aside className='fixed inset-y-0 left-0 z-10 hidden flex-col border-r border-border bg-background sm:flex'>
       <nav className='flex flex-col h-full justify-between items-center w-20 gap-4 py-5'>
-        <AnimateItems
-          className='flex flex-col gap-4'
-          content={siteConfig.adminSidebarItems.map((item, idx) => {
+        <div className='flex flex-col gap-4'>
+          {siteConfig.adminSidebarItems.map((item, idx) => {
             const {
               label,
               icon,
@@ -45,18 +42,16 @@ const AdminSidebar: FC = () => {
               </Tooltip>
             )
           })}
-        />
-        <AnimateItems
-          content={
-            <ButtonWithStatus
-              className='hover:text-destructive-foreground hover:bg-destructive'
-              variant='secondary'
-              onClick={async () =>
-                await signOut()}>
-              <SVG className='size-4' icon={LOGOUT} />
-            </ButtonWithStatus>
-          }
-        />
+        </div>
+        <div className='flex flex-col'>
+          <ButtonWithStatus
+            className='hover:text-destructive-foreground hover:bg-destructive'
+            variant='secondary'
+            onClick={async () =>
+              await signOut()}>
+            <SVG className='size-4' icon={LOGOUT} />
+          </ButtonWithStatus>
+        </div>
       </nav>
     </aside>
   );
